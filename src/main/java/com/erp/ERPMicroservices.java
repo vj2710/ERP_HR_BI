3e0503db35af9dd2f0fe5ab899e40aaea4cba974
akshitaangara <35636606+akshitaangara@users.noreply.github.com>
2018-11-25 15:15:47 -0500

    Both microservices together

---
+++ src/main/java/com/erp/ERPMicroservices.java
@@ -0,0 +1,11 @@
+package com.erp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ERPMicroservices {
+    public static void main(String[] args){
+        SpringApplication.run(ERPMicroservices.class, args);
+    }
+}
